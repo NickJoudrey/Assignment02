1a526aa51136a248a4d87fbf8f844ba4bc9ff4ce
howen2217 <howen2217@HRH-IC0027322.wrdsb.ca>
2016-03-10 08:59:49 -0500

    All questions completed in Assignment 2.

--- src/assignment02/A2Q3.java
+++ src/assignment02/A2Q3.java
@@ -20,59 +20,27 @@ public class A2Q3 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        
+
         City kw = new City();
-        
-        new Wall(kw, 1, 1, Direction.NORTH);
-        new Wall(kw, 1, 1, Direction.WEST);
-        new Wall(kw, 1, 1, Direction.SOUTH);
-        new Wall(kw, 1, 1, Direction.EAST);
-        
-        new Wall(kw, 4, 1, Direction.NORTH);
-        new Wall(kw, 4, 1, Direction.WEST);
-        new Wall(kw, 4, 1, Direction.SOUTH);
-        new Wall(kw, 4, 1, Direction.EAST);
-        
-        new Wall(kw, 1, 4, Direction.NORTH);
-        new Wall(kw, 1, 4, Direction.WEST);
-        new Wall(kw, 1, 4, Direction.SOUTH);
-        new Wall(kw, 1, 4, Direction.EAST);
-        
-        new Wall(kw, 4, 4, Direction.NORTH);
-        new Wall(kw, 4, 4, Direction.WEST);
-        new Wall(kw, 4, 4, Direction.SOUTH);
-        new Wall(kw, 4, 4, Direction.EAST);
-        
-        new Wall(kw, 2, 2, Direction.NORTH);
-        new Wall(kw, 2, 2, Direction.WEST);
-        new Wall(kw, 3, 2, Direction.SOUTH);
-        new Wall(kw, 2, 3, Direction.EAST);
-        new Wall(kw, 2, 3, Direction.NORTH);
-        new Wall(kw, 3, 2, Direction.WEST);
-        new Wall(kw, 3, 3, Direction.SOUTH);
-        new Wall(kw, 3, 3, Direction.EAST);
-        
-        RobotSE dag = new RobotSE (kw, 0, 0, Direction.EAST);
-        
-        dag.move();
-        
-        while(dag.frontIsClear()){
-            dag.move();
-            dag.turnRight();
-            dag.move();
-            
-            if(!dag.frontIsClear()){
-                dag.turnLeft();
+        RobotSE dag = new RobotSE(kw, 5, 7, Direction.WEST);
+
+        while (dag.getAvenue() > 0) {
+
+            if (dag.getDirection() == Direction.WEST) {
                 dag.move();
+            } else {
                 dag.turnLeft();
-                
             }
         }
         
-        
-        
-        
-        
-        
+        while (dag.getStreet() > 0) {
+
+            if (dag.getDirection() == Direction.NORTH) {
+                dag.move();
+            } else {
+                dag.turnLeft();
+            }
+        }
+        dag.turnRight();
     }
 }
